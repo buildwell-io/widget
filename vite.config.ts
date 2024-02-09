640161be0da3317ad3e285f670ac706b15c78a8d
Бабяк Роман Сергійович <roman.babyak@valuetek.com.ua>
2024-02-09 13:46:46 +0200

    refactor: improve `vite` config

--- vite.config.ts
+++ vite.config.ts
@@ -4,46 +4,59 @@ import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
-  root: __dirname,
-  cacheDir: './node_modules/.vite/.',
+    root: __dirname,
+    cacheDir: './node_modules/.vite/.',
 
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
-  plugins: [react(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
-  build: {
-    outDir: './dist/widget',
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+    server: {
+        port: 4200,
+        host: 'localhost',
     },
-  },
 
-  test: {
-    globals: true,
-    cache: {
-      dir: './node_modules/.vitest',
+    preview: {
+        port: 4300,
+        host: 'localhost',
     },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: './coverage/widget',
-      provider: 'v8',
+    plugins: [ react(), nxViteTsPaths() ],
+
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+
+    build: {
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+                pure_funcs: [ 'console.info', 'console.debug', 'console.warn' ],
+            },
+            format: {
+                comments: false,
+                semicolons: false,
+            },
+        },
+        cssCodeSplit: true,
+        outDir: './dist/widget',
+        reportCompressedSize: true,
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
     },
-  },
+
+    test: {
+        globals: true,
+        cache: {
+            dir: './node_modules/.vitest',
+        },
+        environment: 'jsdom',
+        include: [ 'src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}' ],
+
+        reporters: [ 'default' ],
+        coverage: {
+            reportsDirectory: './coverage/widget',
+            provider: 'v8',
+        },
+    },
 });
