1db4da76b96c1271398c97f1de13c8ffde7bc9bc
Бабяк Роман Сергійович <roman.babyak@valuetek.com.ua>
2024-02-12 17:02:33 +0200

    refactor: replace `define` with `env` files

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 * Note: Browsers can block access to window.top due to same origin policy. IE bugs also take place.
 * @see https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy
 * */
-if (!__DEV__&&!(()=>{try{return window.self!==window.top;}catch(e){return true;}})()){
+if (import.meta.env.PROD&&!(()=>{try{return window.self!==window.top;}catch(e){return true;}})()){
     throw new Error('The buildwell.io widget must be placed inside an <iframe> element!');
 }
 
